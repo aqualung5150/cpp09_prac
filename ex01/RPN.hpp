5fd4b60ba1bd223c72362a3adb8f014634560d96
SeungJoonChoi <tmdwns0845@naver.com>
2023-03-19 15:13:05 +0900

    Finish ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -11,23 +11,68 @@ class RPN
 private:
     std::stack<long> _s;
 public:
+    bool invalid_input(std::string input)
+    {
+        std::stringstream in;
+        std::string line;
+        int flag;
+
+        // First two integer
+        if(!isdigit(input[0]) || !isdigit(input[2]))
+            return true;
+
+        in.str(input);
+        line = "";
+
+        std::getline(in, line, ' '); // Skip first integer
+
+        flag = 0;
+        while(std::getline(in, line, ' '))
+        {
+            if(line.length() != 1)
+                return true;
+            else if(!isdigit(line[0]) && line[0] != '+' && line[0] != '-' && line[0] != '*' && line[0] != '/')
+                return true;
+
+            if(isdigit(line[0]))
+                ++flag;
+            else
+                --flag;
+            // To prevent three occurrences in a row
+            if(flag > 2 || flag < 0)
+                return true;
+        }
+        if(flag != 0)
+            return true;
+        return false;
+    }
+
     long calc(const std::string& input)
     {
         std::stringstream in;
         std::string line;
+        int push_count;
+        int oper_count;
         long a;
         long b;
+        bool flag;
 
+        push_count = 0;
+        oper_count = 0;
+        flag = true;
         in.str(input);
         line = "";
         while(std::getline(in, line, ' '))
         {
-            // if(invalid)
-            
-            if(isdigit(line[0]))
-                _s.push(std::strtol(line.c_str(), NULL, 10));
-            else
+            if(invalid_input(input))
             {
+                std::cout << "Error" << std::endl;
+                exit(1);
+            }
+            if(isdigit(line[0]))
+                    _s.push(std::strtol(line.c_str(), NULL, 10));
+            else
+            {        
                 a = _s.top();
                 _s.pop();
                 b = _s.top();
