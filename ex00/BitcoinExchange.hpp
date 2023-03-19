bfb64374d78667d21b097c0511f292625c207225
SeungJoonChoi <tmdwns0845@naver.com>
2023-03-19 16:07:40 +0900

    Remove functions for test

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -19,24 +19,6 @@ public:
     BitcoinExchange& operator=(const BitcoinExchange& b);
 
     int exchange(std::string& line);
-    
-    // Functions for test
-    void testClear()
-    {
-        _database.clear();
-    }
-
-    void testPrint()
-    {
-        std::map<std::string, double>::iterator it;
-
-        it = _database.begin();
-        while(it != _database.end())
-        {
-            std::cout << it->first << " | " << std::setprecision(std::numeric_limits<double>::digits10) << it->second << std::endl;
-            ++it;
-        }
-    }
 };
 
 #endif
