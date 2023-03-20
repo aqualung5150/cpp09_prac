86cfd3993a366bc04a1a098e0d8fbef77b1c88ff
SeungJoonChoi <tmdwns0845@naver.com>
2023-03-20 14:57:40 +0900

    Make insertSort template function

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -8,57 +8,57 @@ class PmergeMe
 private:
     std::vector<unsigned long> _v;
 public:
-    PmergeMe();
-    PmergeMe(const char** argv)
+    PmergeMe()
+    {
+    }
+
+    PmergeMe(char** argv)
     {
         for(int i = 1; argv[i]; ++i)
-            push_back(std::strtoul(argv[i].c_str, NULL, 10));
+            _v.push_back(std::strtoul(argv[i], NULL, 10));
     }
+
+    std::vector<unsigned long>& getV()
+    {
+        return _v;
+    }
+
     /////// SORT ////////////
-
-    // insertionSort
-    /*
-    while(_v[i] < compare)
+    template<typename Iterator>
+    void insertionSort(Iterator& first, Iterator& last)
     {
-        --it;
+        for (Iterator it = first + 1; it != last; it++)
+        {        
+            // Searching the upper bound, i.e., first 
+            // element greater than *it from beginning
+            Iterator const insertion_point = 
+                    std::upper_bound(first, it, *it);
+            
+            // Shifting the unsorted part
+            std::rotate(insertion_point, it, it+1);
+        }
     }
-    if (it != compare)
-    {
-        _v.insert(it, _v[i])
-        _v.erase(i)
-    }
-    */
-
-    // void insertionSort(int first, int last)
-    // {
-    //     for(int i = first; i < last - 1; ++i)
-    //     {
-    //         int j = i + 1;
-    //         std::vector<unsigned long>::iterator it = _v.begin() + i;
-    //         while(j > p && _v[j] < it)
-    //     }
-    // }
 
     //merge
     /*
     */
 
-    void sort(int first, int last)
-    {
-        int middle;
+    // void sort(int first, int last)
+    // {
+    //     int middle;
 
-        last = _v.size() - 1;
-        if (last > 5)
-        {
-            middle = last / 2;
-            sort(first, middle);
-            sort(middle + 1, last);
-            merge(first, middle, last);
-        }
-        else
-            insertionSort(first, last);
-    }
+    //     last = _v.size() - 1;
+    //     if (last > 5)
+    //     {
+    //         middle = last / 2;
+    //         sort(first, middle);
+    //         sort(middle + 1, last);
+    //         merge(first, middle, last);
+    //     }
+    //     else
+    //         insertionSort(first, last);
+    // }
 
-}
+};
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,54 +1,67 @@
-// #include "PmergeMe.hpp"
+#include "PmergeMe.hpp"
 #include <vector>
 #include <iostream>
 #include <algorithm>
 
-void insertionSort(std::vector<int> &vec)
-{
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
-    {        
-        // Searching the upper bound, i.e., first 
-        // element greater than *it from beginning
-        std::vector<int>::iterator const insertion_point = 
-                std::upper_bound(vec.begin(), it, *it);
-          
-        // Shifting the unsorted part
-        std::rotate(insertion_point, it, it+1);        
-    }
-}
-  
 // Function to print the array
-void print(std::vector<int> vec)
+void print(std::vector<unsigned long> vec)
 {
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) 
+    for (std::vector<unsigned long>::iterator it = vec.begin(); it != vec.end(); it++) 
         std::cout << *it << " ";
     std::cout << '\n';
 }
+
+// void insertionSort(std::vector<int> &vec)
+// {
+//     for (std::vector<int>::iterator it = vec.begin() + 1; it != vec.end(); it++)
+//     {        
+//         // Searching the upper bound, i.e., first 
+//         // element greater than *it from beginning
+//         std::vector<int>::iterator const insertion_point = 
+//                 std::upper_bound(vec.begin(), it, *it);
+          
+//         // Shifting the unsorted part
+//         std::rotate(insertion_point, it, it+1);
+//     }
+// }
+  
+
   
 // Driver code
-int main()
-{
-    std::vector<int> arr;
-    arr.push_back(2);
-    arr.push_back(1);
-    arr.push_back(5);
-    arr.push_back(3);
-    arr.push_back(7);
-    arr.push_back(5);
-    arr.push_back(4);
-    arr.push_back(6);
-
-
-    insertionSort(arr);    
-    print(arr);
-}
-
-// int main(int argc, char** argv)
+// int main()
 // {
-//     if (argc < 2) //err
-//         return 1;
+//     std::vector<int> arr;
+//     // 2 1 5 3 7 5 4 6
+//     arr.push_back(2);
+//     arr.push_back(1);
+//     arr.push_back(5);
+//     arr.push_back(3);
+//     arr.push_back(7);
+//     arr.push_back(5);
+//     arr.push_back(4);
+//     arr.push_back(6);
 
-//     PmergeMe a(argv);
+//     std::cout << "before:\t";
+//     print(arr);
 
-//     return 0;
+
+//     insertionSort(arr);
+//     std::cout << "after:\t";
+//     print(arr);
 // }
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) //err
+        return 1;
+
+    PmergeMe a(argv);
+
+    std::vector<unsigned long>::iterator first = a.getV().begin();
+    std::vector<unsigned long>::iterator last = a.getV().end();
+
+    a.insertionSort(first, last);
+    print(a.getV());
+
+    return 0;
+}
